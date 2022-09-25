bfe70006e3ef463d14adbd5ccc65e8db2ab4bcf5
kaynekaynekayne <theygoon@gmail.com>
2022-09-26 02:52:19 +0900

    commit

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,8 +5,8 @@ export const getContacts=(req,res)=>{
     .then(response=>{
         res.header("Access-Control-Allow-Origin","*");
         res.header("Access-Control-Allow-Headers","X-Requested-With");
-        res.status(200).json(response.data);
-        // res.status(200).json(response);
+        // res.status(200).json(response.data);
+        res.status(200).json(response);
     })
     .catch(err=>res.status(400).json({message:err.message}))
 }
