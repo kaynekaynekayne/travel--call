5728147b2fab91c65dc9ba75d3247b57f6911e7f
kaynekaynekayne <theygoon@gmail.com>
2022-09-26 00:39:08 +0900

    put async

--- apis/contact.js
+++ apis/contact.js
@@ -1,13 +1,13 @@
 import instance from ".";
 
-export const getContact=()=>{
+export const getContact=async()=>{
     const response=await instance.get("/",{
         params:{
             serviceKey:process.env.OPEN_API_KEY,
             returnType:JSON,
             numOfRows:10,
             pageNo:1,
-            // [process.env.COUNTRY_NAME]:'일본',
+            [process.env.COUNTRY_NAME]:'일본',
         }
     })
     return response;
