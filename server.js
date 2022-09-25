5cb951a0c0d182a9e8d8c22188d167ab2701ff99
kaynekaynekayne <theygoon@gmail.com>
2022-09-25 16:36:55 +0900

    commit

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 import morgan from 'morgan';
 import cors from 'cors';
+import path from 'path';
 
+const __dirname = path.resolve();
 const app=express();
 
 mongoose.connect(process.env.DB_URL)
@@ -14,6 +16,14 @@ app.use(morgan('dev'));
 app.use(cors({origin:true, credentials:true}));
 app.use(json());
 app.use(urlencoded({extended:false}));
+
+app.get("/", (req,res)=>res.send("haha"));
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'));
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
+
 
 const PORT=4049;
 
