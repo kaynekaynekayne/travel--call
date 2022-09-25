5f72b7f24add2256d3fbebbe9b5f1c4990e4dee0
kaynekaynekayne <theygoon@gmail.com>
2022-09-26 02:47:08 +0900

    commit

--- apis/contact.js
+++ apis/contact.js
@@ -2,14 +2,19 @@
 import axios from 'axios';
 
 export const getContact=async()=>{
-    const response=await axios.get("http://apis.data.go.kr/1262000/LocalContactService2/getLocalContactList2",{
-        params:{
-            serviceKey:process.env.OPEN_API_KEY,
-            returnType:'JSON',
-            numOfRows:10,
-            pageNo:1,
-            // [process.env.COUNTRY_NAME]:'일본',
-        }
-    })
-    return response;
+    let response;
+    try{
+        response=await axios.get("http://apis.data.go.kr/1262000/LocalContactService2/getLocalContactList2",{
+            params:{
+                serviceKey:process.env.OPEN_API_KEY,
+                returnType:'JSON',
+                numOfRows:10,
+                pageNo:1,
+                // [process.env.COUNTRY_NAME]:'일본',
+            }
+        })
+        return response;
+    }catch(err){
+        console.log(err);
+    }
 };
--- client/src/apis/localContact.js
+++ client/src/apis/localContact.js
@@ -2,5 +2,5 @@ import instance from ".";
 
 export const getLocalContact=async()=>{
     const response=await instance.get("/contact/api");
-    return response;
+    return response.data;
 };
