b322b9cada9a8b7e0fdb30fcf2a77e9eb2416a67
kaynekaynekayne <theygoon@gmail.com>
2022-09-25 21:33:43 +0900

    put components

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,5 +1,6 @@
 import Pages from "./pages/pages";
 import {BrowserRouter as Router} from 'react-router-dom';
+import Header from "./components/header";
 
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Pages />
       </Router>
     </div>
