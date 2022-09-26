e3a548cd2ff6d99a1cbb8f7a823bed2e7e3a5381
kaynekaynekayne <theygoon@gmail.com>
2022-09-27 00:33:06 +0900

    create global style

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyles } from './style/globalStyle';
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
---
+++ client/src/style/globalStyle.js
@@ -0,0 +1,16 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const GlobalStyles=createGlobalStyle`
+
+    body{
+        font-family: 'Noto Sans KR', sans-serif;
+    }
+
+    *{
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+        text-decoration: none;
+        color:inherit;
+    }
+`;
