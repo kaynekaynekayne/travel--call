b6c95d1248a49dc839f0cf638284a8047c2c7c48
kaynekaynekayne <theygoon@gmail.com>
2022-09-26 18:11:13 +0900

    string to html

--- client/src/pages/searched.jsx
+++ client/src/pages/searched.jsx
@@ -1,19 +1,51 @@
 import React,{useEffect} from 'react'
+import { useState } from 'react';
 import {useParams} from 'react-router-dom';
 import { getLocalContact } from '../apis/localContact.js'
 
 const Searched = () => {
 
+    const [keyword, setKeyword]=useState("");
+    const [noResult, setNoResult]=useState(false);
+
     const params=useParams();
     const {country}=params;
 
     useEffect(()=>{
-        getLocalContact(country)
-        .then(items=>console.log(items));
+        getContactLists();
     },[params]);
 
+    const getContactLists=async()=>{
+        try{
+            const response=await getLocalContact(country)
+            console.log(keyword);
+            if(response.data.length===0){
+                setNoResult(true);
+            } else{
+                setKeyword(response.data[0]);
+                setNoResult(false);
+            }
+        }catch(err){
+            console.log(err.message);
+        }
+    }
+
+    let ConvertStringToHTML = function(text) {
+        return <div dangerouslySetInnerHTML={{__html:text}}></div>;
+    };
+
     return (
-        <div>searched</div>
+        <div>
+            {noResult ? <h4>결과 없음</h4> :
+            <>
+                <h4>{keyword.country_nm}</h4>
+                <div>{ConvertStringToHTML(keyword.contact_remark)}</div>
+                <img src={keyword.flag_download_url} />
+                <img src={keyword.map_download_url} />
+                <img src={keyword.dang_map_download_url}></img>
+            </> 
+            }
+        </div>
     )
 }
 
