89c4fca994ad206b7aba370c56fec51858dc8699
kaynekaynekayne <theygoon@gmail.com>
2022-10-05 01:13:17 +0900

    add addedPost controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,13 +3,31 @@ import Post from "../models/Post.js";
 export const addPosts=async(req,res)=>{
     try{
         const {email, contactInfo}=req.body;
-        //1. 이메일이 있는지
-        //1-1.이메일이 있으면 
-        const post=await Post.create({
-            email,
-            addedPosts:contactInfo,
-        })
-        return res.status(200).json(post);
+
+        const user=await Post.findOne({email});
+
+        if(user){
+            const {addedPosts}=user;
+            const alreadySavedPosts=addedPosts.find(({country_nm})=>(country_nm===contactInfo.country_nm));
+            if(!alreadySavedPosts){
+                await Post.findByIdAndUpdate(
+                    user._id,
+                    {
+                        addedPosts:[...user.addedPosts, contactInfo],
+                    },
+                    {new:true}
+                )
+            } else{
+                return res.status(400).json({message:"이미 목록에 저장되어 있습니다"})
+            }
+        } else await Post.create({email, addedPosts:[contactInfo]});
+
+        return res.status(200).json({message:"목록에 추가되었습니다"})
+        // const post=await Post.create({
+        //     email,
+        //     addedPosts:contactInfo,
+        // })
+        // return res.status(200).json(post);
     }catch(err){
         res.status(400).json({error:err.message});
     }
