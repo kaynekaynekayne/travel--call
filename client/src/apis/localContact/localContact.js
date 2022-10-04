b9e715919be7918b8c781fdd2b0305eb72bf7e41
kaynekaynekayne <theygoon@gmail.com>
2022-10-04 17:28:31 +0900

    commit

--- client/src/apis/localContact/localContact.js
+++ client/src/apis/localContact/localContact.js
@@ -2,11 +2,9 @@ import instance from "../index.js";
 import axios from 'axios';
 
 export const getLocalContact=async(country)=>{
-    const response=await axios.get("https://travel-call.herokuapp.com/api/contact/"
-    ,{
+    const response=await axios.get("https://travel-call.herokuapp.com/api/contact",{
         params:{country}
-    }
-    );
+    });
     return response.data;
 
 }
