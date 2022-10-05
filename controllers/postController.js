4b5b653184fef4e59e6ebb3c9daa59c294df5aa8
kaynekaynekayne <theygoon@gmail.com>
2022-10-05 22:33:34 +0900

    change controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -10,18 +10,21 @@ export const addPosts=async(req,res)=>{
             const {addedPosts}=user;
             const alreadySavedPosts=addedPosts.find(({country_nm})=>(country_nm===contactInfo.country_nm));
             if(!alreadySavedPosts){
-                await Post.findByIdAndUpdate(
+                const post=await Post.findByIdAndUpdate(
                     user._id,
                     {
                         addedPosts:[...user.addedPosts, contactInfo],
                     },
                     {new:true}
                 )
+                return res.status(200).json({msg:"목록에 추가되었습니다", post})
             } else return res.status(400).json({error:"이미 목록에 저장되어 있습니다"})
 
-        } else await Post.create({email, addedPosts:[contactInfo], uid});
-
-        return res.status(200).json({message:"목록에 추가되었습니다", posts:user.addedPosts})
+        } else {
+            const post=await Post.create({email, addedPosts:[contactInfo], uid});
+            return res.status(200).json({msg:"목록에 추가되었습니다",post})
+        }
+        
         
     }catch(err){
         return res.status(400).json(err);
@@ -33,7 +36,7 @@ export const getPosts=async(req,res)=>{
         const {uid}=req.params;
         const user=await Post.findOne({uid});
         if(user){
-            return res.status(200).json({message:"성공", posts:user.addedPosts})
+            return res.status(200).json({msg:"성공", posts:user.addedPosts})
         } else return res.status(400).json({error:"일치하는 유저를 찾을 수 없습니다"})
     }catch(err){
         return res.status(400).json(err);
