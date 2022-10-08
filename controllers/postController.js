3254002372258268eee42af6181cc149ae463e88
kaynekaynekayne <theygoon@gmail.com>
2022-10-08 17:11:56 +0900

    add sort

--- controllers/postController.js
+++ controllers/postController.js
@@ -34,7 +34,7 @@ export const addPosts=async(req,res)=>{
 export const getPosts=async(req,res)=>{
     try{
         const {uid}=req.params;
-        const user=await Post.findOne({uid});
+        const user=await Post.findOne({uid}).sort({createdAt:-1});
         if(user){
             return res.status(200).json({msg:"성공", posts:user.addedPosts})
         } else return res.status(400).json({error:"일치하는 유저를 찾을 수 없습니다"})
