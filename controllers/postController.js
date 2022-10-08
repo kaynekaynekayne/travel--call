120faa262860d4b617a4f2a92fc516512940f884
kaynekaynekayne <theygoon@gmail.com>
2022-10-08 16:14:42 +0900

    commit

--- controllers/postController.js
+++ controllers/postController.js
@@ -65,7 +65,8 @@ export const removePost=async(req,res)=>{
                 },
                 {new:true}
             )
-            return res.status(200).json({msg:"성공적으로 제거되었습니다", deletedPost}); 
+            return res.status(200).json({msg:"성공적으로 제거되었습니다", addedPosts}); 
+            // return res.status(200).json({msg:"성공적으로 제거되었습니다", deletedPost}); 
         }
     }catch(err){
         return res.status(400).json({error:err});
