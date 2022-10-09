c9f44eeda2def16cc86a416d8aa54e56012effba
kaynekaynekayne <theygoon@gmail.com>
2022-10-09 16:37:19 +0900

    add loading in userlist

--- client/src/pages/userList.jsx
+++ client/src/pages/userList.jsx
@@ -1,25 +1,29 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import { useAuthContext } from '../hooks/useAuthContext';
 import { getAllLists } from '../apis/post/post';
 import { usePostContext } from '../hooks/usePostContext';
 import EachCard from '../components/card';
+import Loading from '../components/loading';
 
 
 const UserList = () => {
 
     const {user}=useAuthContext();
     const {uid}=user;
-
     const {posts, dispatch}=usePostContext();
+    
+    const [loading, setLoading]=useState(false);
 
     useEffect(()=>{
         const fetchLists=async()=>{
+            setLoading(true);
             const response=await getAllLists(uid);
             if(!response.error){
                 dispatch({type:'GET_POSTS', payload:response.data.posts})
             } else{
                 console.log(response.error);
             }
+            setLoading(false);
         }
 
         if(user && uid){
@@ -30,9 +34,11 @@ const UserList = () => {
 
     return (
         <div>
-            {posts && posts.map(post=>
-                <EachCard key={post.country_iso_alp2} post={post}/>
-            )}
+            {loading ? <Loading /> :
+                posts && posts.map(post=>
+                    <EachCard key={post.country_iso_alp2} post={post}/>
+                )
+            }
         </div>
     )
 }
