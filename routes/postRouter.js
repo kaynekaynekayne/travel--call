20356302470dba3a242bd83a90192a94b286ac38
kaynekaynekayne <theygoon@gmail.com>
2022-10-12 14:49:25 +0900

    remove unused function

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { addPosts, beforeAddFirstPost, getPosts, removePost} from '../controllers/postController.js';
+import { addPosts, getPosts, removePost} from '../controllers/postController.js';
 
 const postRouter=express.Router();
 
 postRouter.post("/add", addPosts);
 postRouter.get("/list/:email", getPosts);
 postRouter.put("/delete", removePost);
-postRouter.post("/user",beforeAddFirstPost);
 
 export default postRouter;
