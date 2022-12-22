dade2bb5815b88931cd94559c2e491b7cffe7318
kaynekaynekayne <theygoon@gmail.com>
2022-12-22 16:14:27 +0900

    d-flex

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
 
     return (
-        <div className="d- flex-column justify-content-center align-items-center vh-90">
+        <div className="d-flex flex-column justify-content-center align-items-center vh-90">
             <h5>전세계의 긴급상황 시 연락처를 알려드립니다!</h5>
         </div>
     )
